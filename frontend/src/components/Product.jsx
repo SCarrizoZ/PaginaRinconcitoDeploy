26fa91218b78622a00d66b75c4f7770baed8133d
Sebastián Carrizo <sebastian.carrizo03@inacapmail.cl>
2023-10-25 23:36:54 -0300

    Deploy API busqueda productos

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,15 +1,15 @@
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
 // import Link
 import { Link } from "react-router-dom"
 // Icons
-import {BsPlus, BsEyeFill} from "react-icons/bs"
+import { BsPlus, BsEyeFill } from "react-icons/bs"
 //Context
 import { CartContext } from '../context/CartContext'
 
-export  function Product({product}) {
+export function Product({ product }) {
   const { addToCart } = useContext(CartContext)
   // destructure product's properties
-  const {id,image,category,title,price,user,num_reviews,count_in_stock,created} = product
+  const { id, image, category, name: title, price, user, num_reviews, count_in_stock, created } = product
   return (
     //<div className='w-full h-[300px] bg-pink-200 mb-4' key={id}> {title}</div>
     <div className='rounded-lg border border-[#e4e4e4]'>
@@ -24,15 +24,15 @@ export  function Product({product}) {
           {/**Buttons */}
           <div className='absolute top-6 -right-11 group-hover:right-5 bg-red-500/40 p-2 flex flex-col gap-y-2 justify-center items-center
           opacity-0 group-hover:opacity-100 transition-all duration-300 '>
-            <button onClick={()=>{
-              addToCart(product,id)
+            <button onClick={() => {
+              addToCart(product, id)
             }}>
               <div className='flex justify-center items-center text-white w-12 h-12 bg-red-500'>
-              <BsPlus className="text-3xl"/>
+                <BsPlus className="text-3xl" />
               </div>
             </button>
             <Link to={`/product/${id}`} className='w-12 h-12 bg-white flex justify-center items-center drop-shadow-xl'>
-                <BsEyeFill/>
+              <BsEyeFill />
             </Link>
           </div>
         </div>
@@ -43,10 +43,10 @@ export  function Product({product}) {
         <Link to={`/product/${id}`}>
           <h2 className='font-semibold mb-1'>{title}</h2>
         </Link>
-        
+
         <div className='font-semibold'>${price}</div>
       </div>
-      
+
     </div>
   )
 }
