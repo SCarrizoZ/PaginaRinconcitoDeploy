0c676466de80e31921a3826d9575d5c66f649ab0
Sebastián Alexander Carrizo Zuleta <136368002+SCarrizoZ@users.noreply.github.com>
2023-11-12 18:12:34 -0300

    Delete users directory

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from rest_framework_simplejwt.views import TokenRefreshView
-
-urlpatterns = [
-    path("register/", views.register, name="register"),
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-]
