d914101f9de00245041e74e73218a3cf81d4940e
Edvarddo <eduardo.bautista@inacapmail.cl>
2023-12-15 00:42:40 -0300

    api

--- frontend/config.js
+++ frontend/config.js
@@ -2,5 +2,6 @@
 // export const apiUrl = "https://rinconcito-strapi.onrender.com/api";
 // Imagene secundarias
 export const apiUrl = "https://www.clubdelamusica-pruebas.com/api";
+// asdsa
 // export const apiUrl = "http://localhost:1337/api";
 // export const apiUrl = "http://44.201.172.27/api";
