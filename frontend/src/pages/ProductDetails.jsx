cdc5ede3a4229f05ade94639074c75dd546f848e
Eduardo Alejandro Bautista Lugo <134338695+Edvarddo@users.noreply.github.com>
2023-12-18 22:03:48 -0300

    Update ProductDetails.jsx

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -119,7 +119,16 @@ export const ProductDetails = () => {
 
   // console.log(relatedProducts);
 
-
+  useEffect(() => {
+    const metaTag = 
+      document.createElement('meta');
+      metaTag.setAttribute("property", "og:image");
+      metaTag.content = portada?.data?.attributes?.url;
+      document.getElementsByTagName("head")[0].appendChild(metaTag);
+      return () => {
+        document.getElementsByTagName("head")[0].removeChild(metaTag);
+      }
+  } , [portada?.data?.attributes?.url]);
   return (
     <>
       <div className='flex justify-center'>
@@ -186,30 +195,7 @@ export const ProductDetails = () => {
           </button>
         </div>
 
-      <Helmet>
-        <title>{nombre}</title>
-        {/* <meta name="description" content={descripcion} />
-        <meta name="keywords" content={nombre} />
-        <meta name="author" content="Rinconcito de la Guitarra" /> */}
-        {/* <meta name="robots" content="index, follow" /> */}
-        {/* <meta name="googlebot" content="index, follow" /> */}
-        {/* <meta property="og:title" content={nombre} /> */}
-        {/* <meta property="og:description" content={descripcion} /> */}
-        <meta property="og:image" content={portada?.data?.attributes?.url} />
-        {/* <meta property="og:url" content={`https://rinconcitodelaguitarra.com/product/${id}`} /> */}
-        {/* <meta property="og:site_name" content="Rinconcito de la Guitarra" /> */}
-        {/* <meta property="og:locale" content="es_ES" />
-        <meta property="og:type" content="website" />
-        <meta property="og:updated_time" content="1440432930" />
-        <meta property="fb:app_id" content="966242223397117" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@rinconcitodelaguitarra" />
-        <meta name="twitter:creator" content="@rinconcitodelaguitarra" />
-        <meta name="twitter:title" content={nombre} />
-        <meta name="twitter:description" content={descripcion} /> */}
-        <meta name="twitter:image" content={portada?.data?.attributes?.url} />
-        <meta name="twitter:image:alt" content={nombre} />
-      </Helmet>
+
       </div>
     </>
 
