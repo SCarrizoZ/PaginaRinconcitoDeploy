bc361323dab0c4044202ea19b9ab3004e220f716
Sebastián Carrizo <sebastian.carrizo03@inacapmail.cl>
2024-01-08 22:00:57 -0300

    Corrección cambios a la API

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
-export const apiUrl = "https://rinconcito-api.onrender.com";
+export const apiUrl = "https://rinconcito-api.onrender.com/api";
 // export const apiUrl = "https://www.clubdelamusica-pruebas.com/api";
 
 
